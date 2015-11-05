704093667d208f0668658b0f0cdccbcc33830166
Ray Masaki <ray@raymasaki.com>
2015-11-05 14:22:30 -0500

    Basic sessions working

--- mooooood/app/controllers/sessions_controller.rb
+++ mooooood/app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      redirect_to root_path
     else
       # flash.now[:danger] = "Invalid username/password combination"
       render 'new'
